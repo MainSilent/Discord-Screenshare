2f8e433dd5b7aa1b9676e8f438f959b793266488
Silent <hamidreza.neo@gmail.com>
2021-07-07 16:40:25 +0430

    .

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import time
+import json
+import gzip
+from selenium.webdriver.chrome.options import Options
+from seleniumwire.undetected_chromedriver import Chrome
+
+token = ""
+
+def request_interceptor(request):
+	request.headers['authorization'] = token
+
+def response_interceptor(request, response):
+	print(request)
+
+options = Options()
+driver = Chrome(executable_path="./chromedriver", options=options)
+driver.request_interceptor = request_interceptor
+driver.response_interceptor = response_interceptor
+driver.get('https://discord.com')
