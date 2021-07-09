2cc9708316cb2a2684f3875524b334506cb41883
Silent <hamidreza.neo@gmail.com>
2021-07-09 09:40:43 +0430

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,14 @@ import time
 import json
 import gzip
 from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
-from seleniumwire.undetected_chromedriver import Chrome
+from selenium.webdriver import Chrome, ChromeOptions
 
 url = "http://127.0.0.1:5500"
 
-def response_interceptor(request, response):
-  ...
-	#print(request.url)
-
-options = Options()
+options = ChromeOptions()
 options.add_argument('--disable-web-security')
 options.add_argument('-–allow-file-access-from-files')
-driver = Chrome(executable_path="./chromedriver", chrome_options=options)
-driver.response_interceptor = response_interceptor
+driver = Chrome(executable_path="./chromedriver", options=options)
 driver.get(url)
 
 time.sleep(9999999)
