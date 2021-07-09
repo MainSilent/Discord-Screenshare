95b790930704efddf3a711d0132be7233e5755d5
Silent <hamidreza.neo@gmail.com>
2021-07-09 09:52:41 +0430

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,1 +1,19 @@
-const stream_inject = "sdf"
+function loadJs(filename){
+    var file = document.createElement('script')
+    file.setAttribute("type","text/javascript")
+    file.setAttribute("src", filename)
+    document.getElementsByTagName("head")[0].appendChild(file)
+}
+
+const constraints = {
+    audio: false,
+    video: {
+        width: { max: 1920 },
+        height: { max: 1080 },
+        frameRate: { max: 30 }
+    }
+}
+
+navigator.mediaDevices.getDisplayMedia(constraints).then(stream => {
+    const stream_inject = stream
+})
