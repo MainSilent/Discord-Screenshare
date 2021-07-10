8c11b95bd94db21ba53764c9d7314a73fc62eabb
Silent <hamidreza.neo@gmail.com>
2021-07-10 20:39:16 +0430

    Update stream.py

--- stream.py
+++ stream.py
@@ -16,6 +16,7 @@ class Stream:
 		options = ChromeOptions()
 		options.add_argument('--headless') if headless else None
 		options.add_argument('--no-sandbox')
+		options.add_argument('--window-size=1920,1080')
 		options.add_argument('--disable-web-security')
 		options.add_argument('--disable-dev-shm-usage')
 		options.add_argument('-–allow-file-access-from-files')
