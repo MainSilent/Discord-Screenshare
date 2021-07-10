b7581bb1a93772928916a917a2c268cbb93af2a1
Silent <hamidreza.neo@gmail.com>
2021-07-10 21:17:06 +0430

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from stream import Stream
 
 guild_id = 578558255392096256
-channel_id = 839833386796711957
+channel_id = 856855152555196416
 
 s = Stream(guild_id, channel_id, False)
 s.load_video("./video.mp4")
@@ -10,4 +10,6 @@ s.open_guild()
 s.join()
 s.start()
 
-time.sleep(9999999)
+time.sleep(14)
+
+s.close()
