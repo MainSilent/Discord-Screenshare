68e3f21dceecf3bf905e8efa1d9b22b42318677b
Silent <hamidreza.neo@gmail.com>
2021-07-10 08:01:05 +0430

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ video_path = "./video.mp4"
 
 # Open index page
 options = ChromeOptions()
+options.add_argument('--no-sandbox')
 options.add_argument('--disable-web-security')
 options.add_argument('-–allow-file-access-from-files')
 options.add_argument('--autoplay-policy=no-user-gesture-required')
