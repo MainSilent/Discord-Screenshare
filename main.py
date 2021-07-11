3c7916750a3ed9c0ce393bad50d51ff68942e0cb
Silent <hamidreza.neo@gmail.com>
2021-07-11 20:15:11 +0430

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from stream import Stream
 from discord.ext import commands
 from dotenv import load_dotenv; load_dotenv()
 
-s = Stream(token)
 token = os.getenv("token")
+s = Stream(token)
 bot = commands.Bot(command_prefix='*')
 
 @bot.event
 async def on_ready():
-	print('Ready')
+	print('Bot Ready')
 
 
 @bot.command()
@@ -21,8 +21,9 @@ async def p(ctx):
 	if ctx.message.author.voice is None:
 		return await ctx.send('You need to be in a voice channel to use this command')
 
+	guild_id = ctx.guild.id
+	channel_id = voice.channel.id
 	url = ctx.message.content.split()[1]
-	print(voice)
 
 
 bot.run(token)
