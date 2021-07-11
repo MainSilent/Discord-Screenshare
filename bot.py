93fbd5753c24fd2cdde50e9524b7b2f2ed1370b0
Silent <hamidreza.neo@gmail.com>
2021-07-11 19:25:15 +0430

    .

--- bot.py
+++ bot.py
@@ -1,3 +1,28 @@
-class Bot:
-	def __init__(self, token)
-		self.token = token
+import discord
+
+class Bot(discord.Client):
+	async def on_ready(self):
+		print(self.http.token)
+		print('Logged on as', self.user)
+
+
+
+
+
+
+
+# token = os.getenv("token")
+# guild_id = 859818307870523452
+# channel_id = 859818307870523456
+
+# s = Stream(token, guild_id, channel_id)
+# s.load_video("./tmp/video")
+# s.open_guild()
+# s.join()
+# s.start()
+# s.play()
+
+# time.sleep(20)
+
+# s.pause()
+# s.stop()
--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 import os
 import time
+from bot import Bot
 from stream import Stream
 from dotenv import load_dotenv; load_dotenv()
 
 token = os.getenv("token")
-guild_id = 859818307870523452
-channel_id = 859818307870523456
 
-s = Stream(token, guild_id, channel_id)
-s.load_video("./tmp/video")
-s.open_guild()
-s.join()
-s.start()
-s.play()
-
-time.sleep(20)
-
-s.pause()
-s.stop()
+client = Bot()
+client.run(token)
