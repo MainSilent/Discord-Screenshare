6130acd6cf04ab9927678f032b2d4168e5a0efca
Silent <hamidreza.neo@gmail.com>
2021-07-11 22:25:06 +0430

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ const { Stream } = require('./stream')
 const token = process.env.token
 const stream = new Stream(token)
 
+stream.guild_id = 578558255392096256
+stream.load('./client/tmp/video')
+stream.open_guild()
