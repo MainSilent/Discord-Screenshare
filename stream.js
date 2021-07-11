5ecdd2ad2e0f2aeeb2585077616e3952076acc11
Silent <hamidreza.neo@gmail.com>
2021-07-11 21:51:20 +0430

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,3 +1,6 @@
+const webdriver = require('selenium-webdriver')
+const chrome = require('selenium-webdriver/chrome')
+
 class Video {
 
 }
@@ -5,7 +8,19 @@ class Video {
 class Stream extends Video {
     constructor(token) {
         super()
-        console.log(token);
+        const client_url = `file://${__dirname}/client/index.html`
+        const chrome_options = new chrome.Options()
+        //chrome_options.addArguments('--headless')
+        chrome_options.addArguments('--no-sandbox')
+        chrome_options.addArguments('--window-size=1920,1080')
+        chrome_options.addArguments('--disable-web-security')
+        chrome_options.addArguments('--disable-dev-shm-usage')
+        chrome_options.addArguments('-–allow-file-access-from-files')
+        chrome_options.addArguments('--autoplay-policy=no-user-gesture-required')
+        this.driver = new webdriver.Builder().forBrowser('chrome').setChromeOptions(chrome_options).build()
+
+        this.driver.get(client_url)
+        this.driver.executeScript(`localStorage.setItem("token", '"${token}"')`)
     }
 }
 
