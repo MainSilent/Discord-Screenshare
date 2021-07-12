d3022aafb8ea5ef3b6cdcd3de618486e23322094
Silent <hamidreza.neo@gmail.com>
2021-07-12 11:31:24 +0430

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,20 @@ require('dotenv').config()
 const { Stream } = require('./stream')
 const Discord = require('discord.js')
 
+const prefix = '*'
 const client = new Discord.Client()
 const token = process.env.token
 let stream = new Stream(token)
 
 client.on('ready', () => console.log("Bot started"))
- 
+
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong')
-  }
+    if (msg.content.startsWith(prefix)) {
+        command = msg.content.split(" ")[0].strip("*")
+        console.log(command)
+    }
 })
- 
+
 client.login(token)
 
 // stream.guild_id = "859818307870523452"
